853af12a811dfc60abfa0d0e30779700e95048c1
Efekan <75560188+EfekanSalman@users.noreply.github.com>
2024-07-01 21:47:19 +0300

    Create SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,47 @@
+public class SudokuSolver {
+    public static boolean solve(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+        // Check 3x3 grid
+        int gridRowStart = row - row % 3;
+        int gridColStart = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[gridRowStart + i][gridColStart + j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
